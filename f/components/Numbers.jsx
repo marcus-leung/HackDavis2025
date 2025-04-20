f24f35422daa4c20fb883749abbc0ea19faf86f5
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2025-04-20 07:53:50 -0700

    a

--- f/components/Numbers.jsx
+++ f/components/Numbers.jsx
@@ -4,7 +4,7 @@ export default function Numbers({ fact1, fact2 }) {
   const formatFact = (fact) => {
     const [first, ...rest] = fact.split(' ')
     return (
-      <div className="flex flex-col items-center text-center"> {/* Center-align content */}
+      <div className="flex flex-col items-center text-center"> 
         <span className="text-green-800 text-4xl font-bold font-roboto">{first}</span>
         <span className="text-green-700 text-lg font-bold font-roboto">{rest.join(' ')}</span>
       </div>
@@ -12,12 +12,12 @@ export default function Numbers({ fact1, fact2 }) {
   }
 
   return (
-    <div className="flex flex-col sm:flex-row justify-center gap-8 sm:gap-16 mt-8 mx-4"> {/* Responsive layout */}
-      <div className="flex flex-col items-center text-center"> {/* Center-align each fact */}
-        <p>{formatFact(fact1)}</p>
+    <div className="flex flex-col sm:flex-row justify-center gap-8 sm:gap-16 mt-8 mx-4"> 
+      <div className="flex flex-col items-center text-center"> 
+        {formatFact(fact1)}
       </div>
-      <div className="flex flex-col items-center text-center"> {/* Center-align each fact */}
-        <p>{formatFact(fact2)}</p>
+      <div className="flex flex-col items-center text-center"> 
+        {formatFact(fact2)}
       </div>
     </div>
   )
